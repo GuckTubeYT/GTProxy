54438a4d4cf1a54f9e791c14ea1a57b0e39691e9
Muhammad Kevin <56192597+GuckTubeYT@users.noreply.github.com>
2023-03-09 21:48:53 +0700

    adding to help list

--- events/eventClient.c
+++ events/eventClient.c
@@ -75,7 +75,7 @@ void clientReceive(ENetEvent event, ENetPeer* clientPeer, ENetPeer* serverPeer) 
             if ((packetText + 19)[0] == '/') {
                 char** command = strsplit(packetText + 19, " ", 0);
                 if (isStr(command[0], "/proxyhelp")) {
-                    sendPacket(3, "action|log\nmsg|>> Commands: /helloworld /testarg <your arg> /warp <name world>", clientPeer);
+                    sendPacket(3, "action|log\nmsg|>> Commands: /helloworld /testarg <your arg> /warp <name world> /netid", clientPeer);
                 }
                 else if (isStr(command[0], "/helloworld")) {
                     sendPacket(3, "action|log\nmsg|`2Hello World", clientPeer);
