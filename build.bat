0a908df60a5eeacf6d53044bfcfb5a26ac97a5d4
Muhammad Kevin <56192597+GuckTubeYT@users.noreply.github.com>
2023-04-25 13:41:48 +0700

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
 @echo off
-gcc *.c utils/*.c packet/*.c events/*.c enet/*.c tlse/tlse.c -DTLS_AMALGAMATION -o proxy -lws2_32 -lwinmm -lpthread
+gcc *.c utils/*.c packet/*.c events/*.c enet/*.c tlse/tlse.c md5/md5.c -DTLS_AMALGAMATION -o proxy -lws2_32 -lwinmm -lpthread
 proxy
