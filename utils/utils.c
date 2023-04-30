75ec9cc523010f62d07b43051b06c8af7576a252
Muhammad Kevin <56192597+GuckTubeYT@users.noreply.github.com>
2023-04-30 11:22:11 +0700

    remove fast_compare

--- utils/utils.c
+++ utils/utils.c
@@ -37,40 +37,6 @@ char** strsplit(const char* s, const char* delim, size_t* nb) { // https://githu
     return data;
 }
 
-int fast_compare( const char *ptr0, const char *ptr1, int len) { // https://mgronhol.github.io/fast-strcmp
-    int fast = len/sizeof(size_t) + 1;
-    int offset = (fast-1)*sizeof(size_t);
-    int current_block = 0;
-
-    if( len <= sizeof(size_t)){ fast = 0; }
-
-
-    size_t *lptr0 = (size_t*)ptr0;
-    size_t *lptr1 = (size_t*)ptr1;
-
-    while( current_block < fast ){
-      if( (lptr0[current_block] ^ lptr1[current_block] )){
-        int pos;
-        for(pos = current_block*sizeof(size_t); pos < len ; ++pos ){
-          if( (ptr0[pos] ^ ptr1[pos]) || (ptr0[pos] == 0) || (ptr1[pos] == 0) ){
-            return  (int)((unsigned char)ptr0[pos] - (unsigned char)ptr1[pos]);
-            }
-          }
-        }
-
-      ++current_block;
-      }
-
-    while( len > offset ){
-      if( (ptr0[offset] ^ ptr1[offset] )){
-        return (int)((unsigned char)ptr0[offset] - (unsigned char)ptr1[offset]);
-        }
-      ++offset;
-      }
-
-    return 0;
-}
-
 char* CatchMessage(const char *message, ...) {
     static char* result;
     result = malloc(1024);
